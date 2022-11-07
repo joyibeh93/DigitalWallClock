9ece96f4feec496d2eaa414b94926142c79f1430
joyibeh93 <joychinwenduibeh93@gmail.com>
2022-11-07 17:30:16 +0100

    creating the javascript for the digital wall clock

---
+++ app.js
@@ -0,0 +1,15 @@
+setInterval(currentTime, 1000) {
+    function currentTime() {
+        let time = new Date();
+        let dayDisplay = time.getDay();
+        let hours = time.getHours();
+        let mins = time.getMinutes();
+        let secs = time.getSeconds();
+        let months = time.getMonth();
+        let year = time.getFullYear();
+        let date = time.getDate();
+
+
+    }
+}
+let am_pm = "Am";
